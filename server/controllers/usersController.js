05d26c52882d67e9fc9d5aaedfcdd23f727e141c
Your Name <wallicestenewaweru@gmail.com>
2024-03-14 14:58:15 +0300

    user auth

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,27 +1,35 @@
 import mongoose from "mongoose";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+//function for creating a token for the user
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "10d" });
+};
+
 // login user
 const userLogin = (req, res) => {
   const { email, password } = req.body;
   User.logIn(email, password)
     .then((user) => {
-      res.status(200).json(user);
+      const token = createToken(user._id);
+      res.status(200).json({email, token});
     })
     .catch((error) => {
-      res.status(500).json({error: error.message});
+      res.status(500).json({ error: error.message });
     });
 };
+
 // signUp user
 const userSignUp = (req, res) => {
   const { username, email, password } = req.body;
   User.signUp(username, email, password)
     .then((user) => {
-      res.status(200).json(user);
+      const token = createToken(user._id);
+      res.status(200).json({username, email, token});
     })
     .catch((error) => {
-      res.status(500).json({error: error.message});
+      res.status(500).json({ error: error.message });
     });
 };
-export {
-    userLogin, userSignUp
-}
+export { userLogin, userSignUp };
