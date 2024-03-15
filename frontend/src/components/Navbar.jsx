a4f9c10fc24dc2f7b8b30f84bbf704228bdd3b73
Your Name <wallicestenewaweru@gmail.com>
2024-03-15 19:31:12 +0300

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Logo from "@/assets/Logo";
 import { Search } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -11,26 +12,30 @@ const Navbar = () => {
         <nav>
           <ul className="flex space-x-10">
             <li>
-              <a href="">Home</a>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <a href="">Blog</a>
+              <Link to="/">Blog</Link>
             </li>
             <li>
-              <a href="">Pages</a>
+              <Link to="/">Pages</Link>
             </li>
             <li>
-              <a href="">Contact</a>
+              <Link to="/">Contact</Link>
             </li>
           </ul>
         </nav>
       </div>
       <div className="nav-right w-full">
         <div className=" h-full w-fit rounded bg-gray-50 flex items-center">
-            <input className=" w-full h-full outline-none bg-transparent indent-2 border-none" type="text"  placeholder="Search"/>
-            <div className=" p-1">
-            <Search/>
-            </div>
+          <input
+            className=" w-full h-full outline-none bg-transparent indent-2 border-none"
+            type="text"
+            placeholder="Search"
+          />
+          <div className=" p-1">
+            <Search />
+          </div>
         </div>
       </div>
     </div>
