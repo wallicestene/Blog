eaa72d8fe4dfa02d96e9ad92126346ffee07f0b1
Your Name <wallicestenewaweru@gmail.com>
2024-03-15 23:28:20 +0300

    nav-mobile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import Logo from "@/assets/Logo";
 import { Search } from "@mui/icons-material";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import NavbarMobile from "./NavbarMobile";
 
 const Navbar = () => {
+    const [showNavMobile, setShowNavMobile] = useState(false)
   return (
-    <div className=" flex items-center justify-between h-12 px-4 bg-white shadow">
+    <div className=" relative flex items-center justify-between h-12 px-4 bg-white shadow">
       <div className="nav-left w-full">
         <Logo />
       </div>
-      <div className="nav-center w-full">
+      <div className="nav-center hidden lg:block md:block w-full">
         <nav>
           <ul className="flex space-x-10">
             <li>
@@ -26,7 +30,7 @@ const Navbar = () => {
           </ul>
         </nav>
       </div>
-      <div className="nav-right w-full">
+      <div className="nav-right hidden lg:block md:block w-full">
         <div className=" h-full w-fit rounded bg-gray-50 flex items-center">
           <input
             className=" w-full h-full outline-none bg-transparent indent-2 border-none"
@@ -38,6 +42,18 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      <div className=" lg:hidden md:hidden" onClick={() => setShowNavMobile(!showNavMobile)}>
+        {
+            showNavMobile ? <X/> : <Menu/>
+        }
+      </div>
+      {
+        showNavMobile && (
+           <div className="lg:hidden md:hidden fixed z-10 bg-white top-14 right-0 w-1/2">
+             <NavbarMobile/>
+           </div>
+        )
+      }
     </div>
   );
 };
