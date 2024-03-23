60dd476c99239bd6ca794a0eb39947265e620033
Your Name <wallicestenewaweru@gmail.com>
2024-03-23 14:51:09 +0300

    random Banner item

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,8 +2,22 @@
 import { Alert, Avatar } from "@mui/material";
 import moment from "moment";
 import { Skeleton } from "./ui/skeleton";
+import { useEffect, useState } from "react";
 
 const Banner = ({ data, isLoading, error }) => {
+  const [bannerData, setBannerData] = useState(null);
+  useEffect(() => {
+    const randomBannerData = () => {
+      const index = Math.floor(Math.random() * data.length);
+      setBannerData(data[index]);
+    };
+    // initial call
+    randomBannerData();
+    if (data.length > 1) {
+      const intervalId = setInterval(randomBannerData, 10000);
+      return () => clearInterval(intervalId); // clean up on unmount
+    }
+  }, [data, data.length]);
   return (
     <div className="relative h-[400px] lg:w-[900px] md:w-11/12 w-full rounded-2xl overflow-hidden mb-10 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] z-10 bg-white">
       {isLoading && (
@@ -30,17 +44,17 @@ const Banner = ({ data, isLoading, error }) => {
       {data?.length > 0 && !isLoading && (
         <div className="relative h-full w-full overflow-hidden  ">
           <img
-            src={data[0]?.image}
+            src={bannerData?.image}
             alt=""
             className=" object-cover h-full w-full object-center "
             loading="lazy"
           />
           <div className=" absolute bottom-5 left-2 z-10 p-5 space-y-5">
             <div className=" bg-Primary-500 px-3 py-1 rounded-md first-letter:uppercase text-white font-Open-Sans inline-block tracking-wide">
-              <span>{data[0]?.category}</span>
+              <span>{bannerData?.category}</span>
             </div>
             <h1 className=" font-Gotham-Bold text-3xl font-bold text-white">
-              {data[0]?.title}
+              {bannerData?.title}
             </h1>
             <div className=" flex items-center gap-x-4 text-white font-Gotham-Light text-sm">
               <Avatar
@@ -50,7 +64,7 @@ const Banner = ({ data, isLoading, error }) => {
                 }}
               />
               <p>John Doe</p>
-              <p>{moment(data[0]?.createdAt).format("Do MMM YYYY")}</p>
+              <p>{moment(bannerData?.createdAt).format("Do MMM YYYY")}</p>
             </div>
           </div>
           <div className=" absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-slate-950 from-0% via-slate-900 via-[14%]" />
