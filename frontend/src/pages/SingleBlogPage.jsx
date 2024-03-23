1c3f56b2a47cad160b79478e8087b5aa1e07e4d4
Your Name <wallicestenewaweru@gmail.com>
2024-03-23 21:10:44 +0300

    single-page finished

--- frontend/src/pages/SingleBlogPage.jsx
+++ frontend/src/pages/SingleBlogPage.jsx
@@ -1,5 +1,5 @@
 import useFetch from "@/hooks/useFetch";
-import { Avatar } from "@mui/material";
+import { Alert, Avatar, CircularProgress } from "@mui/material";
 import moment from "moment";
 import { useParams } from "react-router-dom";
 
@@ -9,49 +9,65 @@ const SingleBlogPage = () => {
     `http://localhost:3000/blogs/${id}`
   );
   return (
-    <div className=" mt-[49px] py-2 w-11/12 mx-auto">
-      <div className=" p-2">
-        <div className=" bg-Primary-500 px-3 py-1 rounded-md first-letter:uppercase text-white font-Open-Sans inline-block tracking-wide">
-          <span>{data?.category}</span>
+    <div className=" mt-[49px] py-2 w-11/12 mx-auto space-y-2">
+      {error && (
+        <div>
+          <Alert severity="error">{error}</Alert>
         </div>
-        <h1 className=" my-3 text-2xl tracking-wide font-Gotham-Bold">
-          {data?.title}
-        </h1>
-        <div className=" flex items-center space-x-4 text-gray-500 text-[0.75rem] mt-3 font-Gotham-Light tracking-wide font-bold">
-          <div className=" flex items-center gap-2">
-            <Avatar
-              sx={{
-                width: "30px",
-                height: "30px",
-              }}
-            />
-            <p>John Doe</p>
-          </div>
-          <p>{moment(data?.createdAt).format("Do MMM YYYY")}</p>
+      )}
+      {isLoading && (
+        <div className="flex justify-center items-center h-screen">
+          <CircularProgress />
         </div>
-      </div>
-      <div className="singleMid lg:w-11/12 w-full lg:h-[80vh] h-[50vh] overflow-hidden rounded">
-        <img
-          src={data.image}
-          loading="lazy"
-          alt=""
-          className="w-full h-full object-cover object-center"
-        />
-      </div>
-      <div className="singleBottom">
-        <article>
-          <div className=" w-full space-x-3">
-            {data.body?.map((bodyItem, index) => (
-              <div key={index} className=" space-y-1">
-                <h1 className="text-xl mt-2 leading-7 font-Gotham-Bold text-Secondary-950">
-                  {bodyItem.heading}
-                </h1>
-                <p className=" font-Open-Sans text-md text-Primary-950">{bodyItem.paragraph}</p>
+      )}
+      {data && !isLoading && (
+        <>
+          <div className=" ">
+            <div className=" bg-Primary-500 px-3 py-1 rounded-md first-letter:uppercase text-white font-Open-Sans inline-block tracking-wide">
+              <span>{data?.category}</span>
+            </div>
+            <h1 className=" my-3 text-4xl tracking-wide font-Gotham-Bold leading-7 text-Secondary-950">
+              {data?.title}
+            </h1>
+            <div className=" flex items-center space-x-4 text-Secondary-900  text-[0.75rem] mt-3 font-Gotham-Light tracking-wide font-bold">
+              <div className=" flex items-center gap-2">
+                <Avatar
+                  sx={{
+                    width: "30px",
+                    height: "30px",
+                  }}
+                />
+                <p>John Doe</p>
               </div>
-            ))}
+              <p>{moment(data?.createdAt).format("Do MMM YYYY")}</p>
+            </div>
           </div>
-        </article>
-      </div>
+          <div className="singleMid lg:w-11/12 w-full lg:h-[80vh] h-[50vh] overflow-hidden rounded">
+            <img
+              src={data.image}
+              loading="lazy"
+              alt=""
+              className="w-full h-full object-cover object-center"
+            />
+          </div>
+          <div className="singleBottom">
+            <article>
+              <div className=" w-full space-y-10">
+                {data.body?.map((bodyItem, index) => (
+                  <div key={index} className=" space-y-2">
+                    <h1 className="text-2xl mt-2 font-Gotham-Bold text-Primary-800">
+                      {bodyItem.heading}
+                    </h1>
+                    <p className=" font-Open-Sans leading-7 text-[1.02rem] tracking-wide text-Secondary-900">
+                      {bodyItem.paragraph}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </article>
+          </div>
+        </>
+      )}
     </div>
   );
 };
