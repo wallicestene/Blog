ba13b1c4817775e9420b250f51761f6c89f7af90
Your Name <wallicestenewaweru@gmail.com>
2024-03-24 13:58:05 +0300

    scroll up feature

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ScrollUp from "./components/ScrollUp";
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
+      <ScrollUp/>
       <Outlet />
     </div>
   );
