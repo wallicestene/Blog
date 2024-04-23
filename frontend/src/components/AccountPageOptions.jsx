902eb37bac3269027aa1ef9a7f9ffcba97151863
Your Name <wallicestenewaweru@gmail.com>
2024-04-23 16:51:39 +0300

    account icons

--- frontend/src/components/AccountPageOptions.jsx
+++ frontend/src/components/AccountPageOptions.jsx
@@ -1,11 +1,17 @@
+import { Avatar } from "@mui/material";
+
 /* eslint-disable react/prop-types */
 const AccountPageOptions = ({ Icon, title }) => {
   return (
     <div>
       <div className=" flex items-center space-x-1 px-1 py-2 hover:bg-Secondary-900 hover:transition-colors hover:ease-linear duration-300 delay-75 hover:cursor-pointer rounded tracking-normal font-Gotham-Light">
-        <Icon sx={{
-            fontSize:"1.8rem",
-        }}  />
+        <Avatar
+          sx={{
+            bgcolor: "#4e527f",
+          }}
+        >
+          <Icon />
+        </Avatar>
         <p>{title}</p>
       </div>
     </div>
