af153fa656e833171729162e865417812bb6fccd
Your Name <wallicestenewaweru@gmail.com>
2024-05-03 19:02:39 +0300

    typography3

--- frontend/src/components/BlogComponent.jsx
+++ frontend/src/components/BlogComponent.jsx
@@ -5,6 +5,12 @@ import moment from "moment";
 import { Link } from "react-router-dom";
 
 const BlogComponent = ({ blogData }) => {
+  const extractContent = (htmlContent) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, "text/html");
+    const firstParagraph = doc.querySelector("p"); // Select the first <p> tag
+    return firstParagraph ? firstParagraph.innerHTML.substring(0, 120) : ""; // return text.substring(0, 150); // Display only the first 20 characters
+  };
   return (
     <div className="relative h-96 w-full rounded-lg overflow-hidden shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] group">
       <Link to={`/blogs/single-blog/${blogData?._id}`}>
@@ -15,10 +21,11 @@ const BlogComponent = ({ blogData }) => {
           className=" object-cover w-full h-full transition-transform group-hover:scale-125 delay-50 ease-in-out duration-500 "
         />
         <div className=" absolute bottom-5 left-0 right-0 p-2 z-10 text-white">
-          <h2 className=" my-3 text-2xl tracking-wide font-Gotham-Bold font-bold">
+          <h2 className=" my-1 text-2xl tracking-wide font-Gotham-Bold font-bold">
             {blogData?.title}
           </h2>
-          <div dangerouslySetInnerHTML={{ __html: blogData?.body }} />
+          {/* <div className="blogComp" dangerouslySetInnerHTML={{ __html: blogData?.body }} /> */}
+          <p className=" text-sm font-Open-Sans">{extractContent(blogData?.body) + "..."}</p>
           <div className=" flex items-center justify-between text-[0.75rem] mt-3 font-Gotham-Light">
             <div className=" flex items-center gap-1">
               <Avatar
