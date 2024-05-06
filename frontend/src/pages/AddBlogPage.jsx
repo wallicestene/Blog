ae6224f450f7724816db4ca4ba3599d6b185aad8
Your Name <wallicestenewaweru@gmail.com>
2024-05-06 13:20:06 +0300

    typography4

--- frontend/src/pages/AddBlogPage.jsx
+++ frontend/src/pages/AddBlogPage.jsx
@@ -84,15 +84,15 @@ const AddBlogPage = () => {
       [
         [{ header: 1 }, { header: 2 }],
         ["bold", "italic", "underline", "strike"], // toggled buttons
+
+        [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
         ["blockquote", "code-block"],
         ["link", "image", "video", "formula"],
         [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
         [{ script: "sub" }, { script: "super" }], // superscript/subscript
         [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
         [{ direction: "rtl" }], // text direction
-
-        [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
         [{ color: [] }, { background: [] }], // dropdown with defaults from theme
         [{ font: [] }],
@@ -186,6 +186,7 @@ const AddBlogPage = () => {
               modules={modules}
               value={blogDetails.body}
               onChange={handleBodyChange}
+              style={{ height: "20rem", overflow: "hidden" }}
             />
           </div>
           <div>
